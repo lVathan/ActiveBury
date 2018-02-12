40f9f47f2be0ffa3446fe66c82ae2317d9956e52
Nathan Cordrey <lVathan@rocketmail.com>
2018-02-11 21:24:04 -0500

    small change

--- nathanc/events_calendar.py
+++ nathanc/events_calendar.py
@@ -6,7 +6,7 @@ from flask import Flask, render_template, url_for, json
 def event_reader():
     events= [
     {
-        'start':'2018-2-14','title': 'Valentines Day'
+        'start':'2018-2-14 14:00:00','title': 'Valentines Day'
     },
     {
         'start':'2018-2-17','title': 'Dinner'
