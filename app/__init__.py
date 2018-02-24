5ec4d8e2aabfcf9170eeaab4ae20656d648ac7fa
Nathan Cordrey <lVathan@rocketmail.com>
2018-02-24 12:36:59 -0500

    added followers

--- app/__init__.py
+++ app/__init__.py
@@ -10,6 +10,9 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
 
+from logging.handlers import RotatingFileHandler, SMTPHandler
+import os
+
 
 app = Flask(__name__)
 
@@ -26,5 +29,33 @@ moment = Moment(app)
 
 from app import views
 from app import models
+from app import errors
 #import nathanc.views
 #import nathanc.models
+
+if not app.debug:
+    if app.config['MAIL_SERVER']:
+        auth=None
+        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+        secure = None
+        if app.config['MAIL_USE_TLS']:
+            secure = ()
+        mail_handler = SMTPHandler(
+            mailhost = (app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+            fromaddr = 'no-reply@' + app.config['MAIL_SERVER'],
+            toaddrs = app.config['ADMINS'], subject = 'Web App Failure',
+            credentials = auth, secure=secure)
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
+
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/ncapp.log', maxBytes=10240, backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '[%(asctime)s] %(levelname)s in %(module)s: %(message)s'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('NCApp startup')
